d9f6a62c2d709d6c4af25b845fc3332fc8c20cf6
alegria1026 <yahirhumberto04@gmail.com>
2025-11-06 08:34:30 -0600

    practice 4

--- practica4/js/lab08-test04.js
+++ practica4/js/lab08-test04.js
@@ -1,49 +1,62 @@
 
-// convert comma-separated values into an array
 
+// convert comma-separated values into an array
+const countries = csv.split(",");
 
 // convert array into delimited string
-
+const countriesString = countries.join(" | ");
 
 // check if these are arrays
-
+console.log(Array.isArray(csv));
+console.log(Array.isArray(countries));  
 
 //  sort a simple array 
-
+countries.sort();
+console.log(countries);
 
 // reverse the sort
-
+countries.reverse();
+console.log(countries);
 
 // remove the first element
-
+countries.shift();
+console.log(countries);
 
 // remove the last element
-
+countries.pop();
+console.log(countries);
 
 // add new elements to the front of the array
-
+countries.unshift("Spain", "Brazil");
+console.log(countries);
 
 // search for element
-
+console.log("¿Incluye Germany?:", countries.includes("Germany"));
 
 // search for element index
-
+console.log("Índice de Germany:", countries.indexOf("Germany"));
 
 // make a new array by extracting from another array
-
+const newCountries = countries.splice(1, 3);
+console.log(newCountries);
 
 console.log('---------------------');
 
-// there are other array methods which you will learn in Ch 10 */
-
-
 // use a loop to output all cities whose continent=="NA"
-
+for (let c of cities) {
+  if (c.continent === "NA") {
+    console.log(c.city);
+  }
+}
 
 console.log('---------------------');
 
 // use a loop to output gallery names whose country=="USA"
-
+for (let g of galleries) {
+  if (g.location.country === "USA") {
+    console.log(g.name);
+  }
+}
 
 console.log('---------------------');
 
@@ -51,12 +64,22 @@ console.log('---------------------');
 const colors = JSON.parse(colorsAsString);
 
 // use a loop to output color name if luminance < 75
-
+for (let color of colors) {
+  if (color.luminance < 75) {
+    console.log(color.name);
+  }
+}
 
 console.log('---------------------');
 
 // use two nested loops - outer: loop thru colors
-
+for (let color of colors) {
+  let sum = 0;
+  for (let value of color.rgb) {
+    sum += value;
+  }
+  console.log(`${color.name}: suma RGB = ${sum}`);
+}
 
 console.log('---------------------');
 
@@ -64,8 +87,8 @@ console.log('---------------------');
    list of links to the galleries in the galleries array.
    Make the label of the link the name property, and the href
    the url property */
-
-
-
-
-
+document.write("<ul>");
+for (let g of galleries) {
+  document.write(`<li><a href="${g.url}" target="_blank">${g.name}</a></li>`);
+}
+document.write("</ul>");
